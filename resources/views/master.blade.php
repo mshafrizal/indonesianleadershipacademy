8f3f9e4fead3c4952d77c9b3472b88d0be2dd6aa
MUHAMMAD SHAFRIZAL <muhammad.shafrizal@binus.ac.id>
2019-09-06 02:15:21 +0700

    navbar (not completed)

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,10 +7,11 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Cabin:400,700|Open+Sans:300,400,600&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/8505c87347.js"></script>
         <!-- Styles -->
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
         @include('partials.nav')
@@ -18,5 +19,8 @@
             @yield('content')
         </main>
         @include('partials.footer')
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </body>
 </html>
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-ila">    
+    <div class="container">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link text-uppercase" href="#">About Us <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase" href="#">Gallery</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-uppercase" href="#">Quotes</a>
+                </li>
+                <li class="fb-bg social-item">
+                    <a class="text-uppercase social-link" href="#"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li class="ig-bg social-item">
+                    <a class="text-uppercase social-link" href="#"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li class="twitter-bg social-item">
+                    <a class="text-uppercase social-link" href="#"><i class="fab fa-twitter-square"></i></a>
+                </li>
+                <li class="youtube-bg social-item">
+                    <a class="text-uppercase social-link" href="#"><i class="fab fa-youtube"></i></a>
+                </li>
+                <li class="linkedin-bg social-item">
+                    <a class="text-uppercase social-link" href="#"><i class="fab fa-linkedin"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    
+</nav>
