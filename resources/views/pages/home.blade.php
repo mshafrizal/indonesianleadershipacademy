3adfe949109db2fcac11daa7d4338392d63e665d
MUHAMMAD SHAFRIZAL <muhammad.shafrizal@binus.ac.id>
2019-09-06 00:17:42 +0700

    folder & file preparation

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('master')
+@section('content')
+    <div class="container">
+        <div class="d-flex flex-row justify-content-center align-items-center">
+            <h1>This site is under construction</h1>
+        </div>
+    </div>
+@endsection
